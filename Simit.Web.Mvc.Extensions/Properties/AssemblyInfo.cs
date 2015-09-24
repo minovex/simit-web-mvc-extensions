709da621f455bc2edd607d2a062796089bdbcb5b
Mustafa Ozan Vural <mustafaozanvural@minovex.com>
2015-09-24 13:26:30 +0300

    attiribute set

--- Simit.Web.Mvc.Extensions/Properties/AssemblyInfo.cs
+++ Simit.Web.Mvc.Extensions/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Simit.Web.Mvc.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Minovex")]
 [assembly: AssemblyProduct("Simit.Web.Mvc.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
